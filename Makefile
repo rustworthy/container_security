4d00d772e5ea5ac55e569c00cf5c6cd5fe08eb82
Pavel Mikhalkevich <paulmichellegallery@gmail.com>
2022-12-04 15:25:21 +0300

    Add grafana.

--- Makefile
+++ Makefile
@@ -3,6 +3,12 @@ default: docker/up
 docker/up:
 	chmod +x scripts/compose.sh && ./scripts/compose.sh
 
+docker/up-d:
+	chmod +x scripts/compose.sh && ./scripts/compose.sh -d
+
+docker/restart:
+	docker-compose -f docker/compose.yaml restart
+
 docker/down:
 	docker-compose -f docker/compose.yaml down
 
--- scripts/compose.sh
+++ scripts/compose.sh
@@ -5,6 +5,8 @@ set -e
 [ -x "$(which docker)" ]         || (echo "docker not found" && exit 1)
 [ -x "$(which docker-compose)" ] || (echo "docker-compose not found" && exit 1)
 
+export DOG_API_KEY=${DOG_API_KEY:='123456789'}
+export DOG_GROUP_ID=${DOG_GROUP_ID:=$(getent group nogroup | cut -d: -f3)}
 export DOCKER_GROUP_ID=${DOCKER_GROUP_ID:=$(getent group docker | cut -d: -f3)}
 
-docker-compose -f docker/compose.yaml up
+docker-compose -f docker/compose.yaml up $1
